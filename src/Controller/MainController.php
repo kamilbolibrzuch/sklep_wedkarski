83b2a5e93fd516d2e95fe9e3e542f0e94a0bf533
kamilbolibrzuch <kamil.bolibrzuch@gmail.com>
2020-04-09 17:08:33 +0200

    controller

---
+++ src/Controller/MainController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController;
+{
+  public function index(){
+  return $this->render('home/index.html.twig');
+}
+
+}
