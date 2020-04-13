adb524659a0d471b22ace1649060957fe596a5eb
kamilbolibrzuch <kamil.bolibrzuch@gmail.com>
2020-04-13 18:07:08 +0200

    product add

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-use App\Entity\Product
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,7 +20,7 @@ class ProductController extends AbstractController
  public function show($id)
  {
 
-$product =$this->getDoctrine()->getRepository(Product:class)->find($id);
+$product =$this->getDoctrine()->getRepository(Product::class)->find($id);
 
 if(!$product){
   throw $this->createNotFound('Nie znaleziono produktu o id'.$id);
@@ -29,7 +29,9 @@ return new Response('Zobacz produkt'.$product->getName());
 }
      // or render a template
      // in the template, print things with {{ product.name }}
+     public function index()
+     {
+       return$this->render('product/index.html.twig', ['controller_name'=>'ProductController']);
+     }
      // return $this->render('product/show.html.twig', ['product' => $product]);
- }
-
-}
+ };
