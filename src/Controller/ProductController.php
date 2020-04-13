0f0d78d335b971e686d83d375cda6b8a1a6de525
kamilbolibrzuch <kamil.bolibrzuch@gmail.com>
2020-04-13 19:52:57 +0200

    fixes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 use App\Entity\Product;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,24 +17,23 @@ class ProductController extends AbstractController
  /**
   * @Route("/product/{id}", name="product_show")
   */
-
  public function show($id)
  {
 
-$product =$this->getDoctrine()->getRepository(Product::class)->find($id);
+$product =$this->getDoctrine()->getRepository(Product:class)->find($id);
 
 if(!$product){
   throw $this->createNotFound('Nie znaleziono produktu o id'.$id);
 }
-
-return new Response('Zobacz produkt'.$this->render('product/show.html.twig',['controller_name'=>'ProductController']);
+return new Response('Zobacz produkt'.$product->getName());
 }
-
      // or render a template
      // in the template, print things with {{ product.name }}
      public function index()
      {
-       return$this->render('product/index.html.twig', ['controller_name'=>'ProductController']);
+       return$this->render('product/index.html.twig',['controller_name'=>'ProductController']);
      }
      // return $this->render('product/show.html.twig', ['product' => $product]);
  }
+
+}
